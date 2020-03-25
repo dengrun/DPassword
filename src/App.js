615e3e980b8bb0ed309fdba77fc6ef223fe7b7d5
dengrun <drun@163.com>
2020-03-25 19:10:30 +0800

    增加存储访问

--- src/App.js
+++ src/App.js
@@ -12,28 +12,107 @@ const userSession = new UserSession({ appConfig: appConfig })
 
 export default class App extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      iname: '',
+      ipassword: '',
 
+      passList: []
+    };
+  }
+
+  nameChange(event) {
+    this.setState({ iname: event.target.value })
+  }
+  passChange(event) {
+    this.setState({ ipassword: event.target.value })
+  }
+  fetchData() {
+    const options = { decrypt: false }
+    userSession.getFile('mypass.json', options)
+      .then((file) => {
+        var result = JSON.parse(file || '[]')
+        console.log(result)
+        this.setState({
+          passList: result
+        })
+      })
+      .finally(() => {
+        console.log("read over")
+      })
+  }
+  saveNewPass(data) {
+
+    let mydata = {
+      name: data.name,
+      password: data.password,
+      created_at: Date.now()
+    }
+
+    this.state.passList.push(mydata);
+
+    const options = { encrypt: false }
+    userSession.putFile('mypass.json', JSON.stringify(this.state.passList), options)
+      .then(() => {
+        this.setState({
+          list: this.state.passList
+        })
+      })
+  }
+  doSubmit(event) {
+    let data = {
+      name: this.state.iname,
+      password: this.state.ipassword
+    };
+
+    this.saveNewPass(data);
+  }
   render() {
+    
     return (
       <div>
-        <Header userSession={userSession}/>
-        {
-          userSession.isUserSignedIn() ?
-          <p>登录成功</p>
-          : <p>请登录</p>
-        }
+        <Header userSession={userSession} />
+
+        <span>名称：</span>
+        <input type="text" value={this.state.iusername} onChange={e => this.nameChange(e)}></input>
+        <span>密码：</span>
+        <input type="password" value={this.state.ipassword} onChange={e => this.passChange(e)}></input>
+        <button onClick={e => this.doSubmit(e)}>提交</button>
+
+        <hr></hr>
+
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Password</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.passList.map((v, i) =>
+              <tr>
+                <td>{v.name}</td>
+                <td>{v.password}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
       </div>
-      
+
     );
   }
-  
+
+
   componentDidMount() {
     if (userSession.isSignInPending()) {
       userSession.handlePendingSignIn().then((userData) => {
         window.history.replaceState({}, document.title, "/")
-        this.setState({ userData: userData})
+        this.setState({ userData: userData })
       });
     }
+
+    this.fetchData()
   }
 }
 
